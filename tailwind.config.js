2ebe89c09965877136b3139bd59858af7679e029
abdul-dulal <dulal.dpi.387364@gmail.com>
2024-07-10 14:44:45 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1b1b1b",
+        secondary: "#7ebbf2",
+        base: "#767676",
+      },
+    },
+    screens: {
+      xs: "620px",
+      sm: "768px",
+      md: "880px",
+      lg: "1024px",
+      xl: "1200px",
+      "2xl": "1536px",
+    },
+  },
+  plugins: [],
+};
